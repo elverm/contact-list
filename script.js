cdb868fb46d2cfe07f1840f32811a90c3870e816
elver mendez <elverantoniomendezchico94@gmail.com>
2023-12-21 12:11:39 -0500

    se preparo para el review 1

--- script.js
+++ script.js
@@ -11,9 +11,26 @@ function crearContacto(id, nombres, apellidos, telefono, ciudad, direccion) {
     };
     contactos.push(contacto);
 }
+
+function actualizarContacto (id, nombres, apellidos, telefono, ciudad, direccion){
+contactos[0] = {
+    id: id,
+    nombres: nombres,
+    apellidos: apellidos,
+    telefono: telefono,
+    ubicaciones: { ciudad: ciudad, direccion: direccion }
+};
+}
+
 // Función para eliminar un contacto existente
-function eliminarContacto(id) { contactos = contactos.filter(contacto => contacto.id !== id); }
+function eliminarContacto(id) { 
+    contactos = contactos.filter(contacto => contacto.id !== id); 
+}
+
 // Ejemplo de uso: 
 crearContacto(1, "Juan", "Perez", "123456789", "Lima", "Av. Principal");
 crearContacto(2, "Maria", "Lopez", "987654321", "Cusco", "Calle Principal");
 console.log(contactos);
+actualizarContacto(2, "jose", "Lopez", "987654321", "newYork", "Calle Principal")
+console.log(contactos);
+
